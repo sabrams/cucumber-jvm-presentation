371726a4413d6afab15b2a11914f6c856424eb1e
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-26 12:18:28 -0500

    Implement step definitions

--- src/test/java/cukepresentation/StepDefinitions.java
+++ src/test/java/cukepresentation/StepDefinitions.java
@@ -3,22 +3,66 @@ package cukepresentation;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 import cucumber.runtime.PendingException;
+import junit.framework.Assert;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author: Stephen Abrams
  */
 public class StepDefinitions {
 
+    HttpResponse response;
+    String body;
+
     @When("^a request is made to \"([^\"]*)\"$")
     public void a_request_is_made_to(String arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        HttpClient httpclient = new DefaultHttpClient();
+        String uri;
+        if (arg1.startsWith("/"))
+            uri = "http://localhost:8080" + arg1;
+        else
+            uri = arg1;
+
+        HttpGet httpget = new HttpGet(uri);
+        response = httpclient.execute(httpget);
+
+        httpclient.execute(httpget);
     }
 
     @Then("^the response body should be \"([^\"]*)\"$")
-    public void the_response_body_should_be(String arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+    public void the_response_body_should_be(String expectedBody) throws Throwable {
+        Assert.assertEquals(expectedBody, body());
+    }
+
+    /**
+     *
+     * Use this method to make sure we read the body only once
+     *
+     * @return the response body
+     */
+    private String body() {
+        if (this.body != null)
+            return this.body;
+        this.body = "";
+        try {
+            String line;
+            InputStream is = response.getEntity().getContent();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(is));
+            while ((line = bufferedReader.readLine()) != null)
+                body = body + line + System.getProperty("line.separator");
+        } catch (IOException e) {
+            throw new RuntimeException("Failure while attempting to read HTTP response body", e);
+        }
+        return this.body;
     }
 
 }
