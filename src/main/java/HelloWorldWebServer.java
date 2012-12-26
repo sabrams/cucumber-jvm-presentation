e2eae3fbb1d3dd8794592b355e29474ae66a15c0
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-26 12:58:18 -0500

    Initial web server implementation to be tested, including an unimplemented request handler and its unit tests (T.D.D. step here)

---
+++ src/main/java/HelloWorldWebServer.java
@@ -0,0 +1,21 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * @author: Stephen Abrams
+ */
+public class HelloWorldWebServer {
+
+    public void start() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/hello_world", new HelloWorldHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    public void stop() {
+
+    }
+}
