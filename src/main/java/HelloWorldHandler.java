93ec199f6621f1e5256d59c684e3873a21e03fac
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-26 13:00:28 -0500

    Implement request handler to pass unit tests (T.D.D. phase complete)

--- src/main/java/HelloWorldHandler.java
+++ src/main/java/HelloWorldHandler.java
@@ -7,7 +7,13 @@ import java.io.IOException;
  * @author: Stephen Abrams
  */
 public class HelloWorldHandler implements HttpHandler {
+
+    private static final String helloWorldStr = "Hello World!";
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+
+        httpExchange.sendResponseHeaders(200, helloWorldStr.length());
+        httpExchange.getResponseBody().write(helloWorldStr.getBytes());
     }
 }
