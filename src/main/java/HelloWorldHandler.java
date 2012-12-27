3cb06d076477eea6fe5f4ae608d9c9b96335bb8d
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-27 16:17:33 -0500

    Initial web server implementation to be tested, including an unimplemented request handler and its unit tests (T.D.D. step here)

---
+++ src/main/java/HelloWorldHandler.java
@@ -0,0 +1,13 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+/**
+ * @author: Stephen Abrams
+ */
+public class HelloWorldHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+    }
+}
---
+++ src/test/java/HelloWorldHandlerTest.java
@@ -0,0 +1,65 @@
+import com.sun.net.httpserver.HttpExchange;
+import org.easymock.EasyMock;
+import org.easymock.IArgumentMatcher;
+import org.junit.Test;
+
+import java.io.OutputStream;
+import java.util.Arrays;
+
+/**
+ * @author: Stephen Abrams
+ */
+public class HelloWorldHandlerTest {
+    @Test
+    public void testHandle() throws Exception {
+
+        String expectedResponse = "Hello World!";
+
+        HelloWorldHandler handlerUnderTest = new HelloWorldHandler();
+        HttpExchange mockExchange = EasyMock.createMock(HttpExchange.class);
+
+        // output stream written to
+        OutputStream mockOutputStream = EasyMock.createMock(OutputStream.class);
+
+        // expect to send "OK" response with content length
+        mockExchange.sendResponseHeaders(200, expectedResponse.length());
+        EasyMock.expectLastCall();
+
+        EasyMock.expect(mockExchange.getResponseBody()).andReturn(mockOutputStream);
+
+        // expect "Hello World!" to be written to the output stream
+        mockOutputStream.write(ByteArrayMatcher.eq(expectedResponse.getBytes()));
+        EasyMock.expectLastCall();
+
+        EasyMock.replay(mockExchange, mockOutputStream);
+
+        handlerUnderTest.handle(mockExchange);
+
+        EasyMock.verify(mockExchange, mockOutputStream);
+    }
+
+    public static class ByteArrayMatcher implements IArgumentMatcher {
+
+        byte[] expected;
+
+        public ByteArrayMatcher(byte[] expected) {
+            this.expected = expected;
+        }
+
+        public static byte[] eq(byte[] byteArr) {
+            EasyMock.reportMatcher(new ByteArrayMatcher(byteArr));
+            return null;
+        }
+
+        @Override
+        public boolean matches(Object argument) {
+            byte[] actual = (byte[]) argument;
+            return Arrays.equals(actual, this.expected);
+        }
+
+        @Override
+        public void appendTo(StringBuffer buffer) {
+            buffer.append("eq(").append(this.expected).append(')');
+        }
+    }
+}
