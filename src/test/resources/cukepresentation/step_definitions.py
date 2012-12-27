02698fde1e6e693cdf39b130e73458a3b9907888
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-27 15:27:25 -0500

    Add PENDING step definitions

---
+++ src/test/resources/cukepresentation/step_definitions.py
@@ -0,0 +1,9 @@
+@When('^a request is made to "([^"]*)"$')
+def a_request_is_made_to(self, arg1):
+    # Express the Regexp above with the code you wish you had
+    raise(PendingException())
+
+@Then('^the response body should be "([^"]*)"$')
+def the_response_body_should_be(self, arg1):
+    # Express the Regexp above with the code you wish you had
+    raise(PendingException())
