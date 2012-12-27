2e40950d167dd8840c1e54b052e34246466a1b98
Stephen Abrams <abrams.stephen@gmail.com>
2012-12-27 16:06:26 -0500

    Implement step definitions

--- src/test/resources/cukepresentation/step_definitions.py
+++ src/test/resources/cukepresentation/step_definitions.py
@@ -1,9 +1,23 @@
+from urllib2 import Request, urlopen
+
 @When('^a request is made to "([^"]*)"$')
 def a_request_is_made_to(self, arg1):
-    # Express the Regexp above with the code you wish you had
-    raise(PendingException())
+    if (arg1.startswith("/")):
+        uri = "http://localhost:8080" + arg1
+    else:
+        uri = arg1
+    request = Request(uri, None)
+    self.response = urlopen(request)
+
 
 @Then('^the response body should be "([^"]*)"$')
-def the_response_body_should_be(self, arg1):
-    # Express the Regexp above with the code you wish you had
-    raise(PendingException())
+def the_response_body_should_be(self, expected_body):
+    assert (body(self) == expected_body)
+
+
+def body(self):
+    if (self.response_body is not None):
+        return self.response_body
+    self.response_body = self.response.read()
+    return self.response_body
+
